ebee737f8c6ec53a02e5d41d4e7575d4a7dc5fb0
WuHao <WuHao@wuhao.com>
2025-04-08 19:47:50 +0800

    完成了RPC框架扩展接口 Mock： 使用 mock 服务来模拟远程服务的行为，以便进行接口的测试、开发和调试

--- example-consumer/src/main/java/com/wuhao/example/consumer/ConsumerExample.java
+++ example-consumer/src/main/java/com/wuhao/example/consumer/ConsumerExample.java
@@ -1,6 +1,9 @@
 package com.wuhao.example.consumer;
 
+import com.wuhao.example.common.model.User;
+import com.wuhao.example.common.sercive.UserService;
 import com.wuhao.haorpc.config.RpcConfig;
+import com.wuhao.haorpc.proxy.ServiceProxyFactory;
 import com.wuhao.haorpc.utils.ConfigUtils;
 
 /**
@@ -11,7 +14,18 @@ import com.wuhao.haorpc.utils.ConfigUtils;
  */
 public class ConsumerExample {
     public static void main(String[] args) {
-        RpcConfig rpc = ConfigUtils.loadConfig(RpcConfig.class, "rpc");
-        System.out.println(rpc);
+        // 获取代理
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
+        User user = new User();
+        user.setName("wuhao");
+        // 调用
+        User newUser = userService.getUser(user);
+        if (newUser != null) {
+            System.out.println(newUser.getName());
+        } else {
+            System.out.println("user == null");
+        }
+        long number = userService.getNumber();
+        System.out.println(number);
     }
 }
--- hao-rpc-core/src/main/java/com/wuhao/haorpc/proxy/ServiceProxyFactory.java
+++ hao-rpc-core/src/main/java/com/wuhao/haorpc/proxy/ServiceProxyFactory.java
@@ -1,5 +1,7 @@
 package com.wuhao.haorpc.proxy;
 
+import com.wuhao.haorpc.RpcApplication;
+
 import java.lang.reflect.Proxy;
 
 /**
@@ -17,8 +19,26 @@ public class ServiceProxyFactory {
      * @return
      */
     public static <T> T getProxy(Class<T> serviceClass) {
-        return (T) Proxy.newProxyInstance(serviceClass.getClassLoader(),
-                                            new Class[]{serviceClass},
-                                            new ServiceProxy());
+        if (RpcApplication.getRpcConfig().isMock()) {
+            return getMockProxy(serviceClass);
+        }
+        return (T) Proxy.newProxyInstance(
+                serviceClass.getClassLoader(),
+                new Class[]{serviceClass},
+                new ServiceProxy());
+    }
+
+    /**
+     * 根据服务类获取 Mock 代理对象
+     *
+     * @param serviceClass
+     * @param <T>
+     * @return
+     */
+    public static <T> T getMockProxy(Class<T> serviceClass) {
+        return (T) Proxy.newProxyInstance(
+                serviceClass.getClassLoader(),
+                new Class[]{serviceClass},
+                new MockServiceProxy());
     }
 }
